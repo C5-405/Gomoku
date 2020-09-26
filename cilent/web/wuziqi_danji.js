f03e4dea7f4640ed378202479d523e70c284c459
unknown <1643171221@qq.com>
2020-09-26 14:18:45 +0800

    与后端完成py

--- cilent/web/wuziqi_danji.js
+++ cilent/web/wuziqi_danji.js
@@ -13,36 +13,28 @@ function init() {
             ctx.strokeRect((j * 35) + 1.5, i * 35, 35, 35);
         }
     }
+
+    $.ajax({
+        url: 'http://10.20.62.236:8080/down',
+        type: 'post',
+        data: JSON.stringify({restart: true}),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function (da) {
+            
+        }
+    })
 }
 var black = new Image();
 var white = new Image();
 black.src = "black.png";
 white.src = "white.png";
 var ai = new AI(15);
-  //下棋画子
-  function xia() {
-    if (isWin) {
-        alert('游戏已结束');
-        return;
-    }
-    if (ai.grid[row][col] === undefined) {
-        if (isBlack) {
-            ctx.drawImage(black, col * 35 - 17.5, row * 35 - 17.5);
-            ai.placeAt({ x: row, y: col }, chessColor.black);
-            isWin = ai.evaluate({ x: row, y: col }, chessColor.black) >= 1000000;
-            if (isWin) { alert('黑棋胜'); return }
-            isBlack = false;
-        }
-        else{
-             // var position = ai.thinkDeeply(2, chessColor.white);
-             ctx.drawImage(white, reposition.y* 35 - 17.5, reposition.x* 35 - 17.5);
-             ai.placeAt({x:reposition.x,y:reposition.y} ,chessColor.white);
-             isWin = ai.evaluate({x:reposition.x,y:reposition.y} ,chessColor.white) >= 1000000;
-             if (isWin) { alert('白棋胜'); return }
-             isBlack = true;
-        }
-    } else {
-        alert('不能重复落子');
+
+
+function websockrtse() {
+    if ("WebSocket" in window) {
+        alert
     }
 }
 $(document).ready(function () {
@@ -52,21 +44,64 @@ $(document).ready(function () {
 
         var col = parseInt((e.offsetX + 17.5) / 35);
         var row = parseInt((e.offsetY + 17.5) / 35);
-        xia();
-        var row2=JSON.stringify(row);
-        var col2=JSON.stringify(col);
-        $.post('http://10.20.9.159:8080/test', {
-            x: row2,
-            y: col2
-        },
-            function (data, status) {
-                if(status)
-                {
-                    var reposition = JSON.parse(data);
+        //下黑棋画子
+        function xiahei() {
+            if (isWin) {
+                alert('游戏已结束');
+                return;
+            }
+            if (ai.grid[row][col] === undefined) {
+                if (isBlack) {
+                    ctx.drawImage(black, col * 35 - 17.5, row * 35 - 17.5);
+                    ai.placeAt({ x: row, y: col }, chessColor.black);
+                    isWin = ai.evaluate({ x: row, y: col }, chessColor.black) >= 1000000;
+                    if (isWin) { alert('黑棋胜'); return }
+                    isBlack = false;
+                }
+            } else {
+                alert('不能重复落子');
+            }
+        }
+        xiahei();
+        $.ajax({
+            url: 'http://10.20.62.236:8080/down',
+            type: 'post',
+            data: JSON.stringify({ x: row, y: col }),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (da) {
+                if (isWin) {
+                    alert('游戏已结束');
+                    return;
+                } 
+                if (ai.grid[da.x][da.y] === undefined) {
+                    if (isBlack==false) {
+                        ctx.drawImage(white, da.y * 35 - 17.5, da.x * 35 - 17.5);
+                        ai.placeAt({ x: da.x, y: da.y }, chessColor.white);
+                        isWin = ai.evaluate({ x: da.x, y: da.y }, chessColor.white) >= 1000000;
+                        if (isWin) { alert('白棋胜'); return }
+                        isBlack = true;
+                    }
+                } else {
+                    alert('不能重复落子');
                 }
                 
-            });
+            }
+        })
+      
 
 
-     });
+        // $.post('http://10.20.62.236:8080/down', {
+        //     x:JSON.stringify({x: row}),
+        //     x:JSON.stringify({y: col})
+        // },
+        //     function (data, status) {
+        //         if(status)
+        //         {
+        //             var reposition = JSON.parse(data);
+        //             xia();
+        //         }
+
+        //     });
+    });
 });
